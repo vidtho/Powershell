b39998b8fbdb5072165e98d63a311dc627826c08
vidtho <vid.tho@gmail.com>
2017-09-16 23:59:41 -0500

    new scripts

--- tfs_scripts/CreateFolders.ps1
+++ tfs_scripts/CreateFolders.ps1
@@ -2,6 +2,12 @@
 
 & ".\CreateFolders.ps1" 
 
+Author : Vidya Thotangare
+Version : 1.0
+Purpose: This script will create folder templates in current directory along with sample script files. 
+	 - Folders created for local scrips: z_WIP 
+	 - Folders created for TFS upload : Releases
+	 - All the Schema folders will be replicated from z_WIP
 #>
 
 remove-item ".\Releases" -Force -Recurse
@@ -10,16 +16,6 @@ remove-item ".\Schema2" -Force -Recurse
 remove-item ".\z_WIP" -Force -Recurse
 
 new-item -Path ".\Releases\" -Name Module1 -ItemType directory
-
-new-item -Path ".\Schema1\" -Name Packages -ItemType directory
-new-item -Path ".\Schema1\" -Name Tables -ItemType directory
-new-item -Path ".\Schema1\" -Name SQL -ItemType directory
-new-item -Path ".\Schema1\" -Name Views -ItemType directory
-
-new-item -Path ".\Schema2\" -Name Packages -ItemType directory
-new-item -Path ".\Schema2\" -Name Tables -ItemType directory
-new-item -Path ".\Schema2\" -Name SQL -ItemType directory
-new-item -Path ".\Schema2\" -Name Views -ItemType directory
 
 new-item -Path ".\z_WIP\" -Name Schema1 -ItemType directory
 new-item -Path ".\z_WIP\" -Name Schema2 -ItemType directory
@@ -34,67 +30,98 @@ Add-content -Path ".\z_WIP\CallerScripts\header.sql" @"
 spool xxx.txt
 "@
 
-#==============================================================================
-Add-content -Path ".\Releases\Module1\tfs_caller_script.sql" @"
--- Instructions to execute the script
--- Please send output logfile in email back to xxx_xxx@xxx.com
-
--- Create Log File
-spool xxx.txt
-
-prompt
-prompt Creating table table1
-prompt ======================================
-prompt
-@"..\..\Schema1\Tables\table1.sql"
-prompt
-prompt Creating table table2
-prompt ===================================
-prompt
-@"..\..\Schema1\Tables\table2.sql"
+Add-content -Path ".\z_WIP\CallerScripts\footer.sql" @"
 
 spool off
+
 "@
+
+#==============================================================================
+#Add-content -Path ".\Releases\Module1\tfs_caller_script.sql" @"
+#-- Instructions to execute the script
+#-- Please send output logfile in email back to xxx_xxx@xxx.com
+#
+#-- Create Log File
+#spool xxx.txt
+#
+#prompt
+#prompt Creating table table1
+#prompt ======================================
+#prompt
+#@"..\..\Schema1\Tables\table1.sql"
+#prompt
+#prompt Creating table table2
+#prompt ===================================
+#prompt
+#@"..\..\Schema1\Tables\table2.sql"
+#
+#spool off
+#"@
 #==============================================================================
 Add-content -Path ".\z_WIP\Schema1\sh1_init.sql" @"
-prompt All init data here
+prompt All init1 data here
 prompt ======================
 "@
 
-Add-content -Path ".\z_WIP\Schema1\sh1_vw.sql" @"
-prompt All views
+Add-content -Path ".\z_WIP\Schema1\view1.vw" @"
+prompt create view view11
 prompt ======================
 "@
 
-Add-content -Path ".\z_WIP\Schema1\sh1_table1.tab" @"
-prompt  create table table1
+Add-content -Path ".\z_WIP\Schema1\view2.vw" @"
+prompt create view view12
 prompt ======================
 "@
 
-Add-content -Path ".\z_WIP\Schema1\sh1_table2.tab" @"
-prompt  create table table2
+Add-content -Path ".\z_WIP\Schema1\table1.tab" @"
+prompt  create table table11
 prompt ======================
 "@
 
-Add-content -Path ".\z_WIP\Schema1\sh1_run_template.sql" @"
-prompt Creating table1
+Add-content -Path ".\z_WIP\Schema1\table2.tab" @"
+prompt  create table table12
+prompt ======================
+"@
+
+Add-content -Path ".\z_WIP\Schema1\pkgspec.pks" @"
+prompt  create package pkg1
+prompt ========================
+"@
+
+Add-content -Path ".\z_WIP\Schema1\pkgbody.pkb" @"
+prompt  create package body pkg1
+prompt =========================
+"@
+
+Add-content -Path ".\z_WIP\Schema1\sh1_run.sql" @"
+prompt Creating table table1
+prompt =======================
+prompt
+@@table1.tab
+prompt Creating table table2
 prompt ================
 prompt
-@@sh1_table1.tab
-prompt Creating table2
+@@table2.tab
+prompt
+prompt Creating package pkg1
 prompt ================
 prompt
-@@sh1_table2.tab
+@@pkgspec.pks
 prompt
-prompt Creating package
+prompt Creating package body pkg1
 prompt ================
 prompt
-@@pk_package1.pkb
+@@pkgbody.pkb
 prompt
-prompt creating views
+prompt creating views11
 prompt ================
 prompt
-@@sh1_vw.sql
+@@view1.vw
+prompt
+prompt creating views12
+prompt ================
+prompt
+@@view2.vw
 prompt
 prompt INSERTING init data
 prompt ======================
@@ -104,47 +131,72 @@ prompt
 
 #==============================================================================
 Add-content -Path ".\z_WIP\Schema2\sh2_init.sql" @"
-prompt All init data here
+prompt All init1 data here
 prompt ======================
 "@
 
-Add-content -Path ".\z_WIP\Schema2\sh2_vw.sql" @"
-prompt All views
+Add-content -Path ".\z_WIP\Schema2\view1.vw" @"
+prompt create view view21
 prompt ======================
 "@
 
-Add-content -Path ".\z_WIP\Schema2\sh2_table1.tab" @"
-prompt  create table table1
+Add-content -Path ".\z_WIP\Schema2\view2.vw" @"
+prompt create view view22
 prompt ======================
 "@
 
-Add-content -Path ".\z_WIP\Schema2\sh2_table2.tab" @"
-prompt  create table table2
+Add-content -Path ".\z_WIP\Schema2\table1.tab" @"
+prompt  create table table21
 prompt ======================
 "@
 
-Add-content -Path ".\z_WIP\Schema2\sh2_run_template.sql" @"
-prompt Creating table1
+Add-content -Path ".\z_WIP\Schema2\table2.tab" @"
+prompt  create table table22
+prompt ======================
+"@
+
+Add-content -Path ".\z_WIP\Schema2\pkgspec.pks" @"
+prompt  create package pkg2
+prompt ========================
+"@
+
+Add-content -Path ".\z_WIP\Schema2\pkgbody.pkb" @"
+prompt  create package body pkg2
+prompt =========================
+"@
+
+Add-content -Path ".\z_WIP\Schema2\sh2_run.sql" @"
+prompt Creating table table21
+prompt =======================
+prompt
+@@table1.tab
+prompt Creating table table22
 prompt ================
 prompt
-@@sh2_table1.tab
-prompt Creating table2
+@@table2.tab
+prompt
+prompt Creating package pkg2
 prompt ================
 prompt
-@@sh2_table2.tab
+@@pkgspec.pks
 prompt
-prompt Creating package
+prompt Creating package body pkg2
 prompt ================
 prompt
-@@pk_package1.pkb
+@@pkgbody.pkb
 prompt
-prompt creating views
+prompt creating views21
 prompt ================
 prompt
-@@sh2_vw.sql
+@@view1.vw
+prompt
+prompt creating views22
+prompt ================
+prompt
+@@view2.vw
 prompt
 prompt INSERTING init data
 prompt ======================
 prompt
-@@sh2_init.sql
+@@sh1_init.sql
 "@
---
+++ tfs_scripts/CreateTFSfolders.ps1
@@ -0,0 +1,74 @@
+<# To execute  the scripts in powershell
+
+& ".\CreateTFSfolders.ps1" 
+
+Author : Vidya Thotangare
+Version : 1.0
+Purpose: This script will loop through all schema folders in ".\z_WIP" and will copy the files to TFS respective schema\object folders
+	 - For eg all the table scripts in .\z_WIP\<Schema'n'> will be copied to .\<Schema'n'>\Tables
+	 - This operation will happen for init, view, Packages and Tables scripts.
+
+Assumptions:
+	- All table scripts ends with ".tab"
+	- All view scripts ends with ".vw"
+	- All init scripts ends with "*init.sql"
+	- All Packages scripts ends with ".pks" or ".pks"
+	- All local caller scripts ends with "*run.sql"	
+
+Things to change:
+1. Change the $tfs_script variable to respective Module name [Module1] and script name
+2. Change .\z_WIP\CallerScripts\header.sql with deployment instructions
+#>
+
+
+$exlist = ("CallerScripts")
+$local_folder = ".\z_WIP\"
+
+# Create TFS caller scripts
+$tfs_script = ".\Releases\Module1\module1_tfs.sql"
+remove-item $tfs_script -Force -Recurse
+Get-Content ".\z_WIP\CallerScripts\header.sql" | Set-Content "$tfs_script"
+
+##-===========================================
+## Loop through all .\z_WIP\ Schema folders
+##-===========================================
+ForEach ($dir in (Get-ChildItem -Path $local_folder -Exclude $exlist | ?{$_.PSIsContainer})){
+echo "Generating scripts for : $($dir.name)"
+#echo "$($dir.fullname)"
+
+$fullpath = "$($dir.fullname)"
+$schfolder = "$($dir.name)"
+
+
+    If (Test-Path -Path ".\$schfolder") { 
+	remove-item ".\$schfolder" -Force -Recurse
+	}
+        
+	## Create TFS Schema folders
+	New-Item -Path ".\" -Name "$schfolder" -ItemType Directory | Out-Null
+        New-Item -Path ".\$schfolder" -Name "Packages" -ItemType Directory  | Out-Null
+	New-Item -Path ".\$schfolder" -Name "Tables" -ItemType Directory  | Out-Null
+	New-Item -Path ".\$schfolder" -Name "Views" -ItemType Directory  | Out-Null
+	New-Item -Path ".\$schfolder" -Name "SQL" -ItemType Directory | Out-Null
+
+	## Copy files
+	Copy-Item "$fullpath\*.tab" ".\$schfolder\Tables\"
+	Copy-Item "$fullpath\*.pkb" ".\$schfolder\Packages\"
+	Copy-Item "$fullpath\*.pks" ".\$schfolder\Packages\"
+	Copy-Item "$fullpath\*init.sql" ".\$schfolder\SQL\"
+	Copy-Item "$fullpath\*.vw" ".\$schfolder\Views\"
+
+	## Copy .tab and .vw extensions
+	Dir ".\$schfolder\Tables\*.tab" | rename-item -newname { [io.path]::ChangeExtension($_.name, "sql") }
+	Dir ".\$schfolder\Views\*.vw" | rename-item -newname { [io.path]::ChangeExtension($_.name, "sql") }
+
+	# Append *run.sql to the caller scripts
+	Add-content -path "$tfs_script" -value "Prompt #################################"
+	Add-content -path "$tfs_script" -value "Prompt Scripts for Schema $schfolder"
+	Add-content -path "$tfs_script" -value "Prompt #################################"
+	Add-content -path "$tfs_script" -value "Prompt "
+	Get-Content "$fullpath\*run.sql" | Add-Content "$tfs_script"
+}
+
+Get-Content ".\z_WIP\CallerScripts\footer.sql" | Add-Content "$tfs_script"
+
